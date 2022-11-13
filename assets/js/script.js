72e51d32add1a05ea8db2d074ab553bb0a602849
Chris B <cbega1227@gmail.com>
2022-11-13 08:52:19 -0800

    Trouble shooting Api and working on displaying data to page.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,41 +1,26 @@
+// Bootstrap Carousel
 const myCarouselElement = document.querySelector("#myCarousel");
 const carousel = new bootstrap.Carousel(myCarouselElement, {
   interval: 1000,
   wrap: true,
 });
 
-// Created a variable to store that value of the baseUrl used in the api
-// var baseUrl = "https://api.edamam.com/" + "app_id=" + apiID + "&" + "app_key=" + "$" + apiKey;
-// var baseUrl = "https://api.edamam.com/";
+// Storing base urls for API
 var baseUrl = "https://api.edamam.com/api/recipes/v2?type=public&";
-var baseUrl = "https://api.edamam.com/api/recipes/v2?type=public&";
-
-// console.log(baseUrl);
-var nutrientsUrl = "https://api.edamam.com/api/nutrition-details";
+var nutrientsUrl = "https://api.edamam.com/api/nutrition-details?";
 
 // Created an undefined variable to store the newBaseUrl (newBaseUrl === baseUrl + query (q) + ApiID + ApiKey)
 var newBaseUrl, newNutrientsUrl;
 
-// Created a variable to store a reference to the userSearchInput
-
 // Created a event listener to listen for user event
 $("#search-button").on("click", function () {
   var searchParameter = $("#userSearchInput").val();
-  // console.log(searchParameter);
   concatUrl(searchParameter);
 });
 
 // Created an event listener to listen for click events on random Recipe
-// Using jquery to target any elements with a class of #randomBtn
-//             .on () - event listener
 $("#hangry-button").on("click", function () {
-  // console.log('test');
   let randomSearchParameter = randomRecipe();
-
-  //6 different outputs
-
-  //Random food that will be generated
-  console.log(randomSearchParameter);
   concatUrl(randomSearchParameter);
 });
 
@@ -47,19 +32,15 @@ function randomRecipe() {
   const random = Math.floor(Math.random() * randomRecipeArr.length);
 
   return randomRecipeArr[random];
-  // console.log(randomRecipe[random]);
   // displayResults();
 }
 
-// randomRecipe();
-
 // Created a selectedCriteria function that will be responsible for collecting the selected user criteria
-// parameters to pass - userSearchInput and selectedCriteria
-
 function selectedCriteria() {
   var selectedValues = [];
   var selectedDietValues = [];
 
+  // Checking if values are checked/ or unchecked
   $("input.health:checked").each(function () {
     selectedValues.push(this.value);
   });
@@ -79,15 +60,13 @@ function concatUrl(query, healthLabels, dietLabels) {
   // Created two variables to store the apiID and apiKey
   var apiID = "8b44c5a3";
   var apiKey = "5aa1bbf6b8a35fe5f1a87ae1f373c84d";
-  var apiID = "8b44c5a3";
-  var apiKey = "5aa1bbf6b8a35fe5f1a87ae1f373c84d";
   var nutrientsApiID = "81660670";
   var nutrientsApiKey = "bf3c626930152d5249b50c2db0532e9b";
 
   // Create the concatUrl code to concatenate all passed parameters
   // Reassigned the newBaseUrl variable to store the value of the newly created newBaseUrl
-  newBaseUrl = baseUrl + "q=" + query + "&app_id=" + apiID + "&app_key=" + apiKey;
-  newNutrientsUrl = nutrientsUrl + "q=" + query + "&app_id=" + nutrientsApiID + "&app_key=" + nutrientsApiKey;
+  // newBaseUrl = `${baseUrl}q=${query}&app_id=${apiID}&app_key=${apiKey}`;
+  newNutrientsUrl = `${nutrientsUrl}q=${query}&app_id=${nutrientsApiID}&app_key${nutrientsApiKey}`;
 
   if (healthLabels) {
     healthLabels.map(function (label) {
@@ -99,92 +78,61 @@ function concatUrl(query, healthLabels, dietLabels) {
       newBaseUrl = `${newBaseUrl}&diet=${label}`;
     });
   }
-  console.log(newNutrientsUrl);
-  console.log(newBaseUrl);
-
-  // console.log(newNutrientsUrl);
-  // console.log(newBaseUrl);
-
-  // https://api.edamam.com/api/nutrition-details?app_id=81660670&app_key=bf3c626930152d5249b50c2db0532e9b
-
-  // https://api.edamam.com/api/recipes/v2?type=public&q=salad&app_id=8b44c5a3&app_key=5aa1bbf6b8a35fe5f1a87ae1f373c84d
-
-  // Still need to figure out which functions to call NEXT
-  // Things we need to concat
-  // api ID
-  // api key
-  // baseurl
-  // search parameters
-  // search criteria
-  recipeSearchGetApi(newBaseUrl);
+  // recipeSearchGetApi(newBaseUrl);
+  nutritionGetApi(newNutrientsUrl);
 }
 
-// // Created a recipeSearchApi function that will be responsible for fetching api data
-function recipeSearchGetApi(newBaseUrl) {
-  $.ajax({
-    url: newBaseUrl,
-    method: "GET",
-  }).then(function (response) {
-    // Just creating references to store each key we want to use
-    var hits = response.hits;
-    var cardElements = [];
-
-    for (let i = 0; i < 1; i++) {
-      const element = hits[i];
-      cardElements.push(element.recipe.cuisineType);
-      cardElements.push(element.recipe.dishType);
-      cardElements.push(element.recipe.label);
-      cardElements.push(element.recipe.images.REGULAR.url);
-      cardElements.push(element.recipe.ingredientLines);
-      console.log(cardElements);
-      // // Response object AKA hit
-      // console.log(element.recipe);
-      // // Cuisine Type
-      // console.log(element.recipe.cuisineType);
-      // // High Fiber
-      // console.log(element.recipe.dietLabels);
-      // // Dish Type
-      // console.log(element.recipe.dishType);
-      // // Health Labels
-      // console.log(element.recipe.healthLabels);
-      // // Ingredients
-      // console.log(element.recipe.ingredientLines);
-      // // Recipe image small
-      // console.log(element.recipe.images.SMALL.url);
-      // // Recipe image regular
-      // console.log(element.recipe.images.REGULAR.url);
-      // // Recipe image large
-      // // console.log(element.recipe.images.LARGE.url);
-      // // Title / name of recipe
-      // console.log(element.recipe.label);
-      // // Link to website recipe is from
-      // console.log(element.recipe.url);
-    }
-    // hits is a object of objects and arrays
-    // we can target index of hits[]
-    // console.log(hits[19]);
-  });
-}
-
-// // Calling the recipeSearchGetApi function
-
-// // Created a nutritionApi function that will be responsible for fetching api data
-// function nutritionGetApi() {
-//   // BaseUrl for recipe search API
-//   var baseUrl = "https://api.edamam.com/api/recipes/v2?type=public&";
-
+// Created a recipeSearchApi function that will be responsible for fetching api data
+// function recipeSearchGetApi(newBaseUrl) {
 //   $.ajax({
-//     url: baseUrl,
+//     url: newBaseUrl,
 //     method: "GET",
 //   }).then(function (response) {
-//     console.log(response.hits);
+//     $(response.hits).each(function (index, recipe) {
+//       let dataFromResponse = [];
+//       let storeForLater = [];
+
+//       let title = recipe.recipe.label;
+//       let image = recipe.recipe.images.REGULAR.url;
+//       let cuisine = recipe.recipe.cuisineType[0];
+//       let dishType = recipe.recipe.dishType[0];
+//       let ingredients = recipe.recipe.ingredients;
+//       let ingredientsLine = recipe.recipe.ingredientLines[0];
+//       dataFromResponse.push(title, image, cuisine, ingredients);
+//       storeForLater.push(ingredientsLine);
+//       displayResults(dataFromResponse);
+//     });
 //   });
 // }
-// // Calling the nutritionGetApi function
-// nutritionGetApi();
+// Calling the recipeSearchGetApi function
 
-// // Created a displayResults function that will be responsible for displayingResults
-// function displayingResults() {}
+// Created a nutritionApi function that will be responsible for fetching api data
+function nutritionGetApi() {
+  // BaseUrl for recipe search API
 
-// // RESOURCES ::
-// // ajax () - https://api.jquery.com/jquery.ajax/
+  $.ajax({
+    url: nutrientsUrl,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response.hits);
+  });
+}
+// Calling the nutritionGetApi function
+nutritionGetApi();
+
+// Created a displayResults function that will be responsible for displayingResults
+function displayResults(cardElements) {
+  console.log(cardElements);
+  // 1. I have Api data I want to display
+  // 2. The Api Data has been .push to an new array  (cardElements parameter is that array)
+  // 4. cardElements returns 20 objects containing a : title, img, cuisine, and ingredients
+  // 5. I want to take those 4 items and display each item on a card
+  // a. add each title to a card,
+  // b. add each img to a card,
+  // c. add each cuisine type to a card
+  // d. add each ingredient to the card, but only on hover
+  // 6. I only want to display 6 items until a user clicks the more btn
+}
+
+// RESOURCES ::
+// ajax () - https://api.jquery.com/jquery.ajax/
